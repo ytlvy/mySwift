091fcb6ab3c56411377350cdfc86bff53d9c37d5
zyn <ytlvy2008@gmail.com>
2017-07-01 14:11:27 +0800

    init

---
+++ mySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mySwift
+//
+//  Created by zyn on 2017/7/1.
+//  Copyright © 2017年 ytlvy.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
